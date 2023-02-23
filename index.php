4c7f0f5af2a15fdae7f8f9cf3843236ef588c7b8
llwuyu <2423325630@qq.com>
2023-02-23 16:40:59 +0800

    本地的第一次提交

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * @Author: llwuyu 2423325630@qq.com
+ * @Date: 2023-02-23 16:37:52
+ * @LastEditors: llwuyu 2423325630@qq.com
+ * @LastEditTime: 2023-02-23 16:38:11
+ * @FilePath: \wwwc:\Users\24233\Desktop\pyg\index.php
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+echo 'hello,world!';
